c7ad61f87dab3031f7502bc334925e38d82e30cd
dharlabwustl <atulkumar@wustl.edu>
2024-11-21 23:07:20 -0600

    asdfdsf

--- chapter1/underdetermined_case_example.py
+++
@@ -1,9 +0,0 @@
-
-# Underdetermined Systems: Illustrating the need for constraints
-
-# Define X and Y
-X = [[1, 2, 3], [4, 5, 6]]  # 2x3 matrix
-Y = [[2, 4, 6], [8, 10, 12]]  # 2x3 matrix
-
-# Placeholder for the solution
-print("The system is underdetermined and requires additional constraints to solve for A uniquely.")
